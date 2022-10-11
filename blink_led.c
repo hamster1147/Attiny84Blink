c6968f8358a28d703ca9e36c1b09577018f9d876
Nicholas Aucoin <hamster1147@gmail.com>
2022-10-11 19:35:52 -0400

    Remove debugging bloat

--- blink_led.c
+++ blink_led.c
@@ -16,9 +16,9 @@
 
 #define BLINK_COUNT_MAX 5
 #define BLINK_LED RED_LED
-#define BLINK_INTERVAL_COUNT (3.0 * F_TIMER1)
+#define BLINK_INTERVAL_COUNT (3.0 * F_TIMER1) // 3.0 seconds
 
-static unsigned char overflowed = 0;
+static unsigned char _overflowed = 0;
 
 void ledSet(int port)
 {
@@ -40,16 +40,15 @@ ISR(TIM1_OVF_vect)
 {
    cli(); // Disable global interrupt flag to do work
    ledToggle(OVERFLOW_LED);
-   overflowed = 1;
+   _overflowed = 1;
    sei(); // Re-enable global interrupt flag
 }
 
 int main (void)
 {
-   unsigned char ledOn = 0;
    unsigned char totalBlinks = 0;
-   unsigned long long nextOnCount = BLINK_INTERVAL_COUNT;
-   unsigned long long nextOffCount = BLINK_INTERVAL_COUNT + BLINK_DURATION_COUNT;
+   unsigned long nextOnCount = BLINK_INTERVAL_COUNT;
+   unsigned long nextOffCount = BLINK_INTERVAL_COUNT + BLINK_DURATION_COUNT;
 
    PORTA = 0x0;
    // Set direction of LED pins
@@ -62,18 +61,13 @@ int main (void)
    TCCR1A = 0x0; // Stop Timer
    TCCR1B = 0x0; // Stop Timer
    TCCR1C = 0x0; // Stop Timer
-   TCNT1 = 0x0; // Reset Timer
-   GTCCR = _BV(PSR10); // Reset prescaler
-   OCR1A = 4000; // Set compare to MAX to reset timer
-   OCR1B = 4000;
-   TIMSK1 = 0x0;
-   TIMSK1 |= _BV(TOIE1); // Set Timer1 Compare A Interrupt Enable
-   //TIMSK1 = 0x0;
-   TCCR1B = 0x0;
-   //TCCR1B |= _BV(WGM12); // Enable Clear Timer on Compare mode
+   TCNT1  = 0x0; // Reset Timer
+   GTCCR  = _BV(PSR10); // Reset prescaler
+   OCR1A  = 4000; // Set compare to MAX to reset timer
+   OCR1B  = 4000;
+   TIMSK1 = _BV(TOIE1); // Set Timer1 Compare A Interrupt Enable
    // 1024 Prescalar
    TCCR1B |= _BV(CS10);
-   //TCCR1B |= _BV(CS11);
    TCCR1B |= _BV(CS12);
 
    sei(); // Enable global interrupt flag
@@ -84,34 +78,27 @@ int main (void)
 
    while(1)
    {
-      if (overflowed)
+      if (_overflowed)
       {
          nextOnCount -= TIMER_MAX;
          nextOffCount -= TIMER_MAX;
-         overflowed = 0;
+         _overflowed = 0;
       }
 
-      unsigned long long timer = TCNT1;
+      unsigned long timer = TCNT1;
       if (totalBlinks < BLINK_COUNT_MAX)
       {
-         if (!ledOn)
+         if (timer >= nextOnCount)
          {
-	    if (timer >= nextOnCount)
-	    {
-	       ledSet(BLINK_LED);
-	       nextOnCount = nextOnCount + BLINK_INTERVAL_COUNT;
-	       ledOn = 1;
-            }
-	 }
-         else
+            ledSet(BLINK_LED);
+            nextOnCount = nextOnCount + BLINK_INTERVAL_COUNT;
+         }
+
+         if (timer >= nextOffCount)
          {
-            if (timer >= nextOffCount)
-            {
-               ledClear(BLINK_LED);
-               nextOffCount = nextOffCount + BLINK_INTERVAL_COUNT;
-               totalBlinks++;
-               ledOn = 0;
-            }
+            ledClear(BLINK_LED);
+            nextOffCount = nextOffCount + BLINK_INTERVAL_COUNT;
+            totalBlinks++;
          }
       }
    }
